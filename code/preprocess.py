759f930036fe3ed86af105f5d193b76c316f386c
Varun Chaudhary <Varun_Chaudhary@Trimble.com>
2020-10-25 14:42:30 -0700

    added valid tags- drugs, ade, reason

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,9 +1,9 @@
 # NOTES
 # Remove relations
+# Consider only VALID_TAGS
 # Sort on start
-# Multiple spans in one line
-# random characters in ann file like "" (102027, line 410)
-# Overlap max seq length for bert to preserve context, choose higher logit
+# Multiple spans in one line(take end)
+# Random characters in ann file like "" (102027, line 410)
 
 import os
 import csv
@@ -12,9 +12,27 @@ import pandas as pd
 
 nltk.download('punkt')
 
+ENV = 'train'
+
+SRC = {
+    'train': 'track2-training_data_2',
+    'val': 'track2-training_data_3',
+    'test': 'gold-standard-test-data'
+}
+
+DEST = {
+    'train': 'train',
+    'val': 'val',
+    'test': 'test'
+}
+
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-DATA_DIR = os.path.join(CURR_DIR, '..', 'data', 'track2-training_data_2')
-OUTPUT_DIR = os.path.join(CURR_DIR, '..', 'data', 'train')
+DATA_DIR = os.path.join(CURR_DIR, '..', 'data', SRC[ENV])
+OUTPUT_DIR = os.path.join(CURR_DIR, '..', 'data', DEST[ENV])
+
+SPAN_SIZE = 300
+
+VALID_TAGS = ['Drug', 'Reason', 'ADE']
 
 def get_docs():
     for doc in os.listdir(DATA_DIR):
@@ -47,6 +65,9 @@ def process(patient, txtpath, annpath, outpath):
 
         df = df[df['id'].str.startswith("T")] # filter out R
         df[["tag", "start", "end"]] = df["ann"].str.split(pat=' ', n=2, expand=True) # split ann into tag-start-end
+
+        df = df.loc[df["tag"].isin(VALID_TAGS)] # focus on selected entities
+
         df["end"] = df["end"].apply(lambda x: x.split(" ")[-1]) # take last pos of range(multiple spans when contd on next line)
         df['start'] = df['start'].astype(int) # string -> int
         df['end'] = df['end'].astype(int) # string -> int
@@ -71,12 +92,12 @@ def process(patient, txtpath, annpath, outpath):
                 tokens += currtokens
                 tags += currtags
 
-                currtokens = nltk.word_tokenize(raw_text[annstarts[i]:annends[i] + 1]) # B/I tagged entities
+                currtokens = nltk.word_tokenize(raw_text[annstarts[i]:annends[i]]) # B/I tagged entities
                 currtags = ['B-' + anntags[i]] + ['I-' + anntags[i]] * (len(currtokens) - 1)
                 tokens += currtokens
                 tags += currtags
 
-                start = annends[i] + 1
+                start = annends[i]
 
         currtokens = nltk.word_tokenize(raw_text[start:])
         currtags = ['O'] * len(currtokens)
@@ -88,9 +109,24 @@ def process(patient, txtpath, annpath, outpath):
 
     tokens, tags = get_tokens_tags()
 
+    def get_ids():
+        spanid = 1
+        count = 0
+        ids = []
+        while count < len(tokens):
+            currspan = min(len(tokens) - count, SPAN_SIZE)
+            ids += ([spanid] * currspan)
+            spanid += 1
+            count += currspan
+
+        return ids
+
+    ids = get_ids()
+
     def test():
         print('Running tests')
         assert (len(tokens) == len(tags))
+        assert (len(tokens) == len(ids))
         try:
             for j in range(1, len(tags)):
                 if tags[j][0] == 'I' and tags[j - 1][0] == 'O':
@@ -110,7 +146,7 @@ def process(patient, txtpath, annpath, outpath):
         try:
             with open(outpath, 'w', newline = '') as outfile:
                 writer = csv.writer(outfile)
-                writer.writerows(zip(patientid, tokens, tags))
+                writer.writerows(zip(patientid, ids, tokens, tags))
                 print('File saved - ', outpath)
         except Exception as e:
             print('Error while saving file - ', outpath, e)
